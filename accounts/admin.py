63305bbc220fde91fe97db1c58f3e65db34bfb9a
aslikamat <aslikamat5@gmail.com>
2025-03-16 22:18:01 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser, Parking, Reservation, Review
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'user_type']
+    list_filter = ['user_type']
+    
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Parking)
+admin.site.register(Reservation)
+admin.site.register(Review)
+
