6d4af6412163a17bfcc98b37dbffe53d3b4cc8f5
aslikamat <aslikamat5@gmail.com>
2025-03-17 01:00:48 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,15 +10,9 @@ admin.site.register(Parking)
 admin.site.register(Reservation)
 admin.site.register(Review)
 
-from .models import CustomUser, Parking, Reservation, Review
 
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'user_type']
-    list_filter = ['user_type']
-    
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Parking)
-admin.site.register(Reservation)
-admin.site.register(Review)
+
+
+
 
 
